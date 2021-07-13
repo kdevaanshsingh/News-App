df60c1d9c7ffe3e744970f7c31ac4f6c5dd6c5b1
K. DEVAANSH SINGH <devaanshsingh007@gmail.com>
2021-07-13 12:57:36 +0530

    News App A.K.A Update101

---
+++ app/src/main/java/com/devaanshingh/update101/News.kt
@@ -0,0 +1,6 @@
+package com.devaanshingh.update101
+
+data class News ( val title : String ,
+                  val author : String ,
+                  val url : String ,
+                  val imageUrl : String)
